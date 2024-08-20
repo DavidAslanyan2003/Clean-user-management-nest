fa3c3fb9eae12950973ea6accaab160bfe2db6cb
Mkrtich Arakelyan <mkrtich.arakelyan@zappix.com>
2024-08-20 11:57:28 +0400

    feat[IPD-701]: inline language get

--- src/middlewares/locale.middleware.ts
+++ src/middlewares/locale.middleware.ts
@@ -4,13 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LocaleMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const language = req.headers['Content-language'] as string | undefined;
-
-    if (language) {
-      req['language'] = language.split(',')[0];
-    } else {
-      req['language'] = 'en';
-    }
+    req['language'] = req.headers['content-language'] || 'en';
 
     next();
   }
