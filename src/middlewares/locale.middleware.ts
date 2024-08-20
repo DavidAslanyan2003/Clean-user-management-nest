298cca696013242115bedf6bdae31439d9156a71
Mkrtich Arakelyan <mkrtich.arakelyan@zappix.com>
2024-08-20 11:18:56 +0400

    feat[IPD-701]: fix language params

--- src/middlewares/locale.middleware.ts
+++ src/middlewares/locale.middleware.ts
@@ -4,10 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LocaleMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const acceptLanguage = req.headers['locale'] as string | undefined;
+    const language = req.headers['Content-language'] as string | undefined;
 
-    if (acceptLanguage) {
-      req['language'] = acceptLanguage.split(',')[0];
+    if (language) {
+      req['language'] = language.split(',')[0];
     } else {
       req['language'] = 'en';
     }
