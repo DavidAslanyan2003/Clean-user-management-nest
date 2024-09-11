f4dc1c63d9b88995c8704bc2118dbdcad7a2072a
syuzi-aleksanyan <syuzi.aleksanyan@icketi.com>
2024-09-11 16:39:27 +0400

    feature: [IPD-752] made minor changes in typeorm config file

--- src/database/typeorm.ts
+++ src/database/typeorm.ts
@@ -28,9 +28,9 @@ export const createDataSourceOptions = (
     type: 'redis',
     options: {
       host: configService.get<string>('REDIS_HOST'),
-      port: configService.get<string>('REDIS_PORT'),
-      username: 'my-redis',
-      password: 'my-redis',
+      port: configService.get<number>('REDIS_PORT'),
+      username: configService.get<string>('REDIS_USERNAME'),
+      password: configService.get<string>('REDIS_PASSWORD'),
     },
     duration: configService.get<number>('REDIS_DURATION'),
   },
